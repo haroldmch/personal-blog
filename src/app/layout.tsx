fc1b7576721fcd471c1ceffbbb329aa1d171f8e9
Harold Muñoz <haroldmunoz@MacBook-Air-de-Harold.local>
2024-04-20 16:42:06 -0500

    Nextjs installed

---
+++ src/app/layout.tsx
@@ -0,0 +1,21 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Personal Blog",
+  description: "Blog personal de HaroldMCh",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
