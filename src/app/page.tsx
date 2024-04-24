9dc0543f5485b25afbdd6aae129be02f64cc7733
Harold Muñoz <haroldmunoz@MacBook-Air-de-Harold.local>
2024-04-23 22:39:53 -0500

    SASS and React Icons added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,11 @@
+import { SiDungeonsanddragons } from 'react-icons/si';
+
+import styles from './page.module.scss';
+
 export default function Home() {
-  return <main>Testing</main>;
+  return (
+    <main className={styles.test}>
+      Testing <SiDungeonsanddragons />
+    </main>
+  );
 }
